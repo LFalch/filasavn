5100ee2f562fe8bc58859f55fedf6d954438a558
LFalch <lucas@wasd.dk>
2025-06-03 01:14:34 +0200

    Populate tree

---
+++ src/lib.rs
@@ -0,0 +1,101 @@
+use std::{fs::{self, File}, io::{self, BufRead, ErrorKind, Read, Write}, path::Path};
+
+pub type Savn = Vec<FileSpec>;
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+pub enum FileType {
+    #[default]
+    RegularFile = 1,
+    ExecutableFile = 2,
+    SoftSymlink = 3,
+}
+
+pub struct FileSpec {
+    pub path: Box<str>,
+    pub file_type: FileType,
+    pub contents: Box<[u8]>,
+}
+
+pub fn read_savn_or_empty(savn_path: impl AsRef<Path>) -> io::Result<Savn> {
+    match read_savn(savn_path) {
+        Err(e) if e.kind() == ErrorKind::NotFound => Ok(Savn::new()),
+        res => res,
+    }
+}
+pub fn read_savn(savn_path: impl AsRef<Path>) -> io::Result<Savn> {
+    let mut f = io::BufReader::new(File::open(savn_path)?);
+    let mut savn = Savn::new();
+
+    loop {
+        let mut path = Vec::with_capacity(8);
+        f.read_until(0, &mut path)?;
+        // pop final zero
+        path.pop();
+        if path.is_empty() {
+            // if the name is empty that means we've reached the end of the archive
+            break;
+        }
+        let path = String::from_utf8(path)
+            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?
+            .into_boxed_str();
+        let mut b = [0];
+        f.read_exact(&mut b)?;
+        let file_type = match b {
+            [1] => FileType::RegularFile,
+            [3] => FileType::SoftSymlink,
+            _ => return Err(io::Error::new(io::ErrorKind::InvalidData, "unknown file type")),
+        };
+        let mut b = 0u32.to_le_bytes();
+        f.read_exact(&mut b)?;
+        let size = u32::from_le_bytes(b);
+        let mut contents = vec![0; size as usize];
+        f.read_exact(&mut contents)?;
+        let contents = contents.into_boxed_slice();
+
+        savn.push(FileSpec { path, file_type, contents })
+    }
+
+    Ok(savn)
+}
+pub fn write_savn(savn: &Savn, savn_path: impl AsRef<Path>) -> io::Result<()> {
+    let mut f = io::BufWriter::new(File::create(savn_path)?);
+
+    for spec in savn {
+        f.write_all(spec.path.as_bytes())?;
+        f.write_all(&[0])?;
+        f.write_all(&[spec.file_type as u8])?;
+        f.write_all(&(spec.contents.len() as u32).to_le_bytes())?;
+        f.write_all(&spec.contents)?;
+    }
+    // write final zero to indicate archive end
+    f.write_all(&[0])?;
+
+    Ok(())
+}
+pub fn find_file<'a>(savn: &'a Savn, file: &str) -> Option<&'a FileSpec> {
+    for entry in savn {
+        if &*entry.path == file {
+            return Some(entry);
+        }
+    }
+    None
+}
+pub fn add_file(savn: &mut Savn, file: impl AsRef<Path>) -> io::Result<()> {
+    let path = file.as_ref();
+
+    let metadata = fs::symlink_metadata(path)?;
+    let mut file_type = FileType::default();
+    let contents = if metadata.is_symlink() {
+        file_type = FileType::SoftSymlink;
+        fs::read_link(path)?.to_str().unwrap().as_bytes().to_vec().into_boxed_slice()
+    } else {
+        fs::read(path)?.into_boxed_slice()
+    };
+
+    savn.push(FileSpec {
+        path: path.display().to_string().into_boxed_str(),
+        file_type,
+        contents,
+    });
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,106 @@
+use std::{env::args, fs, path::PathBuf};
+
+use filasavn::{add_file, read_savn_or_empty, write_savn, FileSpec, FileType::{ExecutableFile, RegularFile, SoftSymlink}};
+
+enum Command<I: Iterator<Item=String>> {
+    List,
+    Add(I),
+    Remove(I),
+    Extract(String),
+}
+
+fn main() -> Result<(), String> {
+    let mut args = args();
+    let this_exe = args.next().unwrap();
+    let archive = args.next().ok_or_else(|| format!("no archive given"))?;
+    let command = args.next().ok_or_else(|| format!("no command given"))?;
+
+    let cmd = match &*command {
+        "-h" | "help" => {
+            println!("syntax:");
+            println!("\t{this_exe} <archive> list");
+            println!("\t{this_exe} <archive> add <files..>");
+            println!("\t{this_exe} <archive> remove <files..>");
+            println!("\t{this_exe} <archive> extract <target folder>");
+            return Ok(())
+        },
+        "list" => {
+            if args.len() > 0 {
+                return Err(format!("list does not take any arguments"));
+            }
+            Command::List
+        }
+        "add" => Command::Add(args),
+        "remove" => Command::Remove(args),
+        "extract" => {
+            let target = args.next().ok_or_else(|| format!("no target folder given"))?;
+            if args.len() > 0 {
+                return Err(format!("list does not take any arguments"));
+            }
+            Command::Extract(target)
+        }
+        _ => return Err(format!("unknown command")),
+    };
+    drop(command);
+    drop(this_exe);
+    let mut savn = read_savn_or_empty(&archive).map_err(|e| e.to_string())?;
+
+    match cmd {
+        Command::List => {
+            let mut max_length_width = 0;
+            for FileSpec{contents, ..} in &savn {
+                let length_width = contents.len().ilog10() as usize + 1;
+                max_length_width = max_length_width.max(length_width);
+            }
+            for spec in &savn {
+                match spec.file_type {
+                    RegularFile => print!("f"),
+                    ExecutableFile => print!("x"),
+                    SoftSymlink => print!("l"),
+                }
+                print!(" {:width$} ", spec.contents.len(), width = max_length_width);
+                println!("{}", spec.path);
+            }
+        }
+        Command::Add(paths) => {
+            for path in paths {
+                add_file(&mut savn, path).map_err(|e| e.to_string())?;
+            }
+            write_savn(&savn, archive).map_err(|e| e.to_string())?;
+        }
+        Command::Remove(paths) => {
+            let set: Vec<_> = paths.collect();
+            savn.retain(|spec| !set.iter().any(|p| **p == *spec.path));
+            write_savn(&savn, archive).map_err(|e| e.to_string())?;
+        }
+        Command::Extract(target) => {
+            let target = PathBuf::from(target);
+            
+            for file_spec in savn {
+                let path = target.join(&*file_spec.path);
+                let mut path2 = path.clone();
+                path2.pop();
+                fs::create_dir_all(path2).map_err(|e| e.to_string())?;
+                match file_spec.file_type {
+                    RegularFile => fs::write(path, file_spec.contents).map_err(|e| e.to_string())?,
+                    SoftSymlink => {
+                        #[cfg(windows)]
+                        {
+                            compile_error!("todo");
+                        }
+                        #[cfg(unix)]
+                        {
+                            use std::ffi::OsStr;
+
+                            let original: &OsStr = std::os::unix::ffi::OsStrExt::from_bytes(&file_spec.contents);
+                            std::os::unix::fs::symlink(original, path).map_err(|e| e.to_string())?;
+                        }
+                    },
+                    ExecutableFile => todo!(),
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
